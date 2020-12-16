43fe80be88e18e329acc23b1cfa4e2aa45619396
Sergei Panin <sergeipanin1993@gmail.com>
2020-12-16 15:59:33 +0300

    Initial commit

---
+++ src/main/java/command/ZipAddCommand.java
@@ -0,0 +1,29 @@
+package command;
+
+import exception.PathIsNotFoundException;
+import main.ConsoleHelper;
+import main.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipAddCommand extends ZipCommand {
+    public void execute() throws Exception {
+            try {
+                ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+
+                ZipFileManager zipFileManager = getZipFileManager();
+
+                ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+                Path sourcePath = Paths.get(ConsoleHelper.readString());
+
+                zipFileManager.addFile(sourcePath);
+
+                ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
+            } catch (PathIsNotFoundException e) {
+                ConsoleHelper.writeMessage("Файл не был найден.");
+            }
+
+    }
+}
